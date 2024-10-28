c1a1eaf7b0b7da22fe0c3921aff9b783098d814f
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2024-10-28 16:45:13 -0700

    Make RH more resistant to connection closure (will make testing Envoy easy)

--- src/main/java/kafkarequesttool/requests/ReadHelper.java
+++ src/main/java/kafkarequesttool/requests/ReadHelper.java
@@ -4,17 +4,12 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Reads a response (its 4-byte length header and N following bytes).
  *
  * @author adam.kotwasinski
  */
 public class ReadHelper {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ReadHelper.class);
 
     private static final int HEADER_SIZE = 4;
 
@@ -26,36 +21,48 @@ public class ReadHelper {
         this.db = null;
     }
 
-    public void read(final SocketChannel channel)
-            throws IOException {
-
-        while (this.hb.hasRemaining()) {
-            channel.read(this.hb);
-        }
-        this.hb.flip();
-        final int dataLength = this.hb.getInt();
-        LOG.trace("Response length = {}", dataLength);
-
-        if (dataLength >= 0) {
-            this.db = ByteBuffer.allocate(dataLength);
-            while (this.db.hasRemaining()) {
-                channel.read(this.db);
-            }
-            this.db.flip();
-        }
-        else {
-            throw new IllegalArgumentException("No data: " + dataLength);
-        }
-    }
-
     public static ByteBuffer receive(final SocketChannel channel) {
         final ReadHelper rh = new ReadHelper();
         try {
-            rh.read(channel);
-            return rh.db;
+            return rh.read(channel);
         }
         catch (final IOException e) {
             throw new IllegalArgumentException("IO failure", e);
+        }
+    }
+
+    private ByteBuffer read(final SocketChannel channel)
+            throws IOException {
+
+        readFullBuffer(channel, this.hb);
+
+        this.hb.flip();
+        final int dataLength = this.hb.getInt();
+
+        // Empty response has very little sense.
+        if (dataLength <= 0) {
+            throw new IOException("Invalid response data length: " + dataLength);
+        }
+
+        this.db = ByteBuffer.allocate(dataLength);
+        readFullBuffer(channel, this.db);
+
+        this.db.flip();
+        return this.db;
+    }
+
+    private static void readFullBuffer(final SocketChannel channel, final ByteBuffer target)
+            throws IOException {
+
+        while (target.hasRemaining()) {
+            final int read = channel.read(target);
+            if (-1 == read) {
+                // End of stream.
+                break;
+            }
+        }
+        if (target.hasRemaining()) {
+            throw new IOException("Buffer underflow");
         }
     }
 
